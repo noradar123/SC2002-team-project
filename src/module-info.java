689fdae689918ea48d72cec3fd7dbe4416787181
noradar123 <wesley20051022@gmail.com>
2025-11-21 08:35:59 +0800

    final

--- src/module-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * The main module for the SC2002 Team Project (LabAssignment).
- * Contains the application logic for the Internship Management System, 
- * including controllers, views, and entity models.
- */
-module LabAssignment {
-}
